5a2803cbf3e7ae5bc2c3b606e5bd0f78802b85e6
tlsdnwn55 <tlsdnwn55@naver.com>
2022-07-08 10:52:57 +0900

    feat: 프로젝트 디렉토리 구조 설정
    - sample class 포함

---
+++ Lotte_SweetPatatos_Warr-Cinema/src/movie/MovieContorller.java
@@ -0,0 +1,32 @@
+package movie;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MovieContorller extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+//	final Optional<String> param = Optional.ofNullable(req.getParameter("param"));
+//	
+//	if(param.isEmpty()) {
+//		resp.sendRedirect("index");
+//	}
+//	
+	
+	
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+	
+	
+
+}
